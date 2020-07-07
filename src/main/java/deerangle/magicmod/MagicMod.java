b9e5f4296ed46a8e0da0b244ea203a3a68920ef7
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2020-07-07 23:57:44 +0200

    renamed file to magicmod

--- src/main/java/deerangle/magicmod/MagicMod.java
+++ src/main/java/deerangle/magicmod/MagicMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package deerangle.magicmod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,8 +19,8 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("magicmod")
+public class MagicMod
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
@@ -54,7 +54,7 @@ public class ExampleMod
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("magicmod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
