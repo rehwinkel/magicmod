6b927a07614d5b664c0783cd7fb208e795bbeb9d
Dommee100 <dominic.woehrle@gmx.de>
2020-07-08 16:56:31 +0200

    Added Amethyst Ore

--- src/main/java/deerangle/magicmod/block/BlockRegistry.java
+++ src/main/java/deerangle/magicmod/block/BlockRegistry.java
@@ -15,19 +15,24 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 public class BlockRegistry {
 
     public static Block WAND_TABLE;
+    public static Block AMETHYST_ORE_BLOCK;
 
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event) {
         WAND_TABLE = new WandTableBlock(Block.Properties.create(Material.ROCK, MaterialColor.BLUE))
                 .setRegistryName("wand_table");
-        event.getRegistry().registerAll(WAND_TABLE);
+        AMETHYST_ORE_BLOCK = new Block(Block.Properties.create(Material.ROCK, MaterialColor.PURPLE))
+                .setRegistryName("amethyst_ore");
+        event.getRegistry().registerAll(WAND_TABLE, AMETHYST_ORE_BLOCK);
     }
 
     @SubscribeEvent
     public static void registerBlockItems(RegistryEvent.Register<Item> event) {
         BlockItem WAND_TABLE_ITEM = new BlockItem(WAND_TABLE, new Item.Properties().group(MagicMod.tab));
         WAND_TABLE_ITEM.setRegistryName(WAND_TABLE.getRegistryName());
-        event.getRegistry().registerAll(WAND_TABLE_ITEM);
+        BlockItem AMETHYST_ORE_ITEM = new BlockItem(AMETHYST_ORE_BLOCK, new Item.Properties().group(MagicMod.tab));
+        AMETHYST_ORE_ITEM.setRegistryName(AMETHYST_ORE_BLOCK.getRegistryName());
+        event.getRegistry().registerAll(WAND_TABLE_ITEM, AMETHYST_ORE_ITEM);
     }
 
 }
