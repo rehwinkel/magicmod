60e96442a73c45c2c1f6f01aa64ebb2c74c3fcae
Dommee100 <dominic.woehrle@gmx.de>
2020-07-08 18:01:48 +0200

    Added World Gen for Amethyst

--- src/main/java/deerangle/magicmod/main/MagicMod.java
+++ src/main/java/deerangle/magicmod/main/MagicMod.java
@@ -1,24 +1,34 @@
 package deerangle.magicmod.main;
 
 import deerangle.magicmod.item.ItemRegistry;
+import deerangle.magicmod.world.WorldGen;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.biome.Biome;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("magicmod")
 public class MagicMod {
-    
+
     public static final ItemGroup tab = new ItemGroup("magicmod") {
 
         @Override
         public ItemStack createIcon() {
             return new ItemStack(ItemRegistry.MASTER_WAND);
         }
-        
-    };
-    
-    public MagicMod() {
 
+    };
+
+    public MagicMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(FMLCommonSetupEvent event) {
+        for (Biome b : Biome.BIOMES) {
+            WorldGen.addAmethystOre(b);
+        }
     }
 
 }
---
+++ src/main/java/deerangle/magicmod/world/WorldGen.java
@@ -0,0 +1,22 @@
+package deerangle.magicmod.world;
+
+import deerangle.magicmod.block.BlockRegistry;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStage.Decoration;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.feature.OreFeatureConfig.FillerBlockType;
+import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraft.world.gen.placement.Placement;
+
+public class WorldGen {
+
+    public static final OreFeatureConfig amethystOreConfig = new OreFeatureConfig(FillerBlockType.NATURAL_STONE,
+            BlockRegistry.AMETHYST_ORE.getDefaultState(), 6);
+
+    public static void addAmethystOre(Biome biome) {
+        biome.addFeature(Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(amethystOreConfig)
+                .withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(2, 5, 15, 45))));
+    }
+
+}
