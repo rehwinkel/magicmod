e1a3c99d7aff212707c991f879cd877d61b4346e
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2020-07-09 22:29:18 +0200

    added engraved stone tablet

--- src/main/java/deerangle/magicmod/block/StoneTabletBlock.java
+++ src/main/java/deerangle/magicmod/block/StoneTabletBlock.java
@@ -9,10 +9,12 @@ import net.minecraft.world.IBlockReader;
 
 public class StoneTabletBlock extends Block {
 
+    private final boolean enchanted;
     private static final VoxelShape SHAPE = Block.makeCuboidShape(1.0, 0.0, 1.0, 15.0, 4.0, 15.0);
 
-    public StoneTabletBlock(Properties properties) {
+    public StoneTabletBlock(Properties properties, boolean enchanted) {
         super(properties);
+        this.enchanted = enchanted;
     }
 
     @Override
