3b9de889bb38af1ccbf85b30397d74885d762b4d
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2020-07-12 13:35:43 +0200

    pedestal now drops contents and items don't intersect with model anymore

--- src/main/java/deerangle/magicmod/block/PedestalBlock.java
+++ src/main/java/deerangle/magicmod/block/PedestalBlock.java
@@ -4,9 +4,12 @@ import deerangle.magicmod.block.entity.PedestalTileEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
@@ -29,6 +32,21 @@ public class PedestalBlock extends Block {
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return SHAPE;
+    }
+
+    @Override
+    public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
+        if (!state.isIn(newState.getBlock())) {
+            IItemHandler inventory = worldIn.getTileEntity(pos)
+                    .getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).orElse(null);
+            NonNullList<ItemStack> items = NonNullList.create();
+            for (int i = 0; i < inventory.getSlots(); i++) {
+                items.add(i, inventory.getStackInSlot(i));
+            }
+            InventoryHelper.dropItems(worldIn, pos, items);
+
+            super.onReplaced(state, worldIn, pos, newState, isMoving);
+        }
     }
 
     @Override
--- src/main/java/deerangle/magicmod/proxy/ClientProxy.java
+++ src/main/java/deerangle/magicmod/proxy/ClientProxy.java
@@ -24,7 +24,7 @@ public class ClientProxy implements Proxy {
         ClientRegistry.bindTileEntityRenderer(TileEntityRegistry.WAND_TABLE,
                 dispatcher -> new ItemStandTileEntityRenderer(dispatcher, 27.0 / 32.0, 0.7F, true));
         ClientRegistry.bindTileEntityRenderer(TileEntityRegistry.PEDESTAL,
-                dispatcher -> new ItemStandTileEntityRenderer(dispatcher, 15.0 / 16.0, 0.5F, false));
+                dispatcher -> new ItemStandTileEntityRenderer(dispatcher, 31.0 / 32.0, 6F / 16F, false));
         ScreenManager.registerFactory(ContainerRegistry.WAND_TABLE, WandTableGui::new);
     }
 
